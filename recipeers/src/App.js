afaa78862f9d8c4b3ef60ac64bedc8f105d9d491
Amanda Freeman <mandafae5@gmail.com>
2018-08-23 23:06:19 -0500

    set up basic express app and connect server

--- recipeers/src/App.js
+++ recipeers/src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 import Landing from "./components/landing";
 
 class App extends Component {
+  state = {
+    response: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/hello");
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
   render() {
-    return <Landing />;
+    return (
+      <div>
+        <h1>{this.state.response}</h1>
+        <Landing />
+      </div>
+    );
   }
 }
 
